4530b36259755a0043a2d76112b7b83156c6a4f2
SoraTenshi <dream@neoncity.dev>
2024-03-04 00:18:01 +0100

    Added more stuff.
    istg handling jstring types is an absolute pain.

--- src/java/This.java
+++ src/java/This.java
@@ -4,8 +4,18 @@ public class This {
   }
 
   private native void hello();
+  private native int add(final int lhs, final int rhs);
+  private native void print(final String target);
 
   public static void main(String[] arg) {
-    new This().hello();
+    final var t = new This();
+    System.out.println("\n------------------- START -------------------");
+    t.hello();
+
+    System.out.format("Java says: calling t.add(13, 37) -> %d\n", t.add(13, 37));
+
+    t.print("Hello Ziggy (from your \"beloved\" friend Java) :)");
+
+    System.out.println("-------------------- END --------------------\n");
   }
 }
--- src/main.zig
+++ src/main.zig
@@ -6,3 +6,14 @@ const jni = @cImport({
 pub export fn Java_This_hello(_: ?*jni.JNIEnv, _: jni.jobject) callconv(.C) void {
     std.io.getStdOut().writer().print("Hello from: {s}\n", .{"Zig!"}) catch unreachable;
 }
+
+pub export fn Java_This_add(_: ?*jni.JNIEnv, _: jni.jobject, lhs: jni.jint, rhs: jni.jint) callconv(.C) jni.jint {
+    return lhs + rhs;
+}
+
+pub export fn Java_This_print(this: *jni.JNIEnv, _: jni.jobject, target: jni.jstring) callconv(.C) void {
+    // Bruh is this ugly, but compatibility says no
+    const target_str = (this.*.*.GetStringUTFChars orelse return)(this, target, null);
+
+    std.io.getStdOut().writer().print("Zig says: {s}\n", .{target_str}) catch unreachable;
+}
